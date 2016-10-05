f52c722f92b808bb2c98cc3b3b2b05ff11a7d84c
trebek1 <trebek08@gmail.com>
2016-10-04 19:20:47 -0700

    added delte method

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux'; 
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router'; 
 
 class PostsShow extends Component {
 
+	static contextTypes = {
+		router: PropTypes.object
+	};
+
 	componentWillMount(){
 		this.props.fetchPost(this.props.params.id); 
+	}
+
+	handleDelete(){
+		this.props.deletePost(this.props.params.id)
+		.then(() => {
+			this.context.router.push('/');
+		})
+
 	}
 
 	render(){
@@ -19,6 +32,8 @@ class PostsShow extends Component {
 				<h3> {post.title} </h3>
 				<h6> Categories {post.categories} </h6>
 				<p> {post.content} </p>
+				<Link className="btn btn-primary" to="/"> Back to Posts </Link>
+				<button onClick={this.handleDelete.bind(this)} className="btn btn-danger"> Delete </button>
 			</div>
 
 			)
@@ -29,4 +44,4 @@ function mapStateToProps(state){
 	return { post: state.posts.post };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow); 
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow); 
