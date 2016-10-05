f79c9869b5fd3af3fe2515aeb43df9f114f7f6bd
trebek1 <trebek08@gmail.com>
2016-10-04 18:33:48 -0700

    added links to individual posts

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,15 @@ import App from './components/app';
 
 import PostsIndex from './components/posts_index'; 
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 // Index route -> if the route matches the parent, show index
 export default (
 	<Route path="/" component={App} >
 		<IndexRoute component={PostsIndex}/>
 		<Route path="posts/new" component={PostsNew} />
+		<Route path="posts/:id" component={PostsShow} />
+
 	</Route>
 );
 
